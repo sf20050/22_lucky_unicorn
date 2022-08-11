bf6af7c18d7333b2b0354afd6eedaf42e6a23efd
sf20050 <sf20050@melville-high.school.nz>
2022-08-11 14:20:31 +1200

    added yes no, instructions and number check function

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -25,6 +25,26 @@ def instructions():
     return ""
 
 
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask question
+            response = int(input(question))
+            # if the amount is too low / too high to give
+            if low < response <= high:
+                return response
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # main route
 played_before = yes_no("Have you play this game before? ")
 
@@ -33,3 +53,7 @@ if played_before == "no":
 
 print("program continues")
 
+# ask how much they want to play with
+
+how_much = num_check("How much would you like to play with? ", 0, 10)
+print("You have asked to play with ${}".format(how_much))
